cef5db95b619ae6488176233b73934b48425f880
Qual1dea <liksan@inbox.ru>
2022-05-20 23:07:22 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import numpy as np
 
 try:
+    np.set_printoptions(precision=3, linewidth=150)
     start = time.time()
     N = int(input('Введите количество строк/столбцов матрицы > 3: '))
     while N < 4:
@@ -9,9 +10,8 @@ try:
     K = int(input('Введите число К:'))
     A = np.random.randint(-10, 10, (N, N))        # задаем матрицу A
     F = np.copy(A)                                # задаем матрицу F
-    time_next = time.time()
     print('Матрицa A:\n', A)
-    a, b = 0, 0                       # переменная для подсчета количество нулей в C в нечетных столбцах
+    a, b = 0, 1                       # переменная для подсчета количество нулей в C в нечетных столбцах
     for i in range(N):
         for j in range(N):
             if i < (N // 2) and j > (N // 2 - (N - 1) % 2):
@@ -27,7 +27,7 @@ try:
         for i in range(N // 2):
             for j in range(N // 2):
                 F[i][j], F[i + N // 2 + N % 2][j + N // 2 + N % 2] = F[i + N // 2 + N % 2][j + N // 2 + N % 2], F[i][j]
-    print('Матрицa F:',F)
+    print('Матрицa F:\n', F)
 
     if np.linalg.det(A) > sum(np.diagonal(F)):
         if np.linalg.det(A) == 0:
@@ -38,7 +38,7 @@ try:
         if np.linalg.det(F) == 0 or np.linalg.det(np.tril(A)) == 0:
             print("Матрица F или G вырожденная, дальнейшие вычисления невозможны")
         else:
-            print (f'Нижняя Треугольная Матрица G:\n{np.tril(A)}')
+            print(f'Нижняя Треугольная Матрица G:\n{np.tril(A)}')
             print(f"A+G^(-1)-F^(-1))*K\n{(A + np.linalg.inv(np.tril(A)) - np.linalg.inv(F)) * K}")
     print(f"Programm time: {time.time()-start}")
 except ValueError:
